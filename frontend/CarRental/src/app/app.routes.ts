871d9f869968f172b3457c8b66929b95473505c3
Przemek5901 <pbednarz10@gmail.com>
2024-05-08 19:12:44 +0200

    added routing

--- frontend/CarRental/src/app/app.routes.ts
+++ frontend/CarRental/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //   {
+  //     path: '',
+  //     redirectTo: 'profil?¿',
+  //     pathMatch: 'full',
+  //   },
+  {
+    path: 'pojazdy/dodaj',
+    loadComponent: () =>
+      import('./vehicles/add-vehicle/add-vehicle.component').then(
+        (m) => m.AddVehicleComponent
+      ),
+  },
+  {
+    path: 'pojazdy/przegladaj',
+    loadComponent: () =>
+      import('./vehicles/browse-vehicles/browse-vehicles.component').then(
+        (m) => m.BrowseVehiclesComponent
+      ),
+  },
+  {
+    path: 'pojazdy/przegladaj',
+    loadComponent: () =>
+      import('./vehicles/edit-vehicles/edit-vehicles.component').then(
+        (m) => m.EditVehiclesComponent
+      ),
+  },
+  //   {
+  //     path: '**',
+  //     component: komponentprofilu,
+  //   },
+];
