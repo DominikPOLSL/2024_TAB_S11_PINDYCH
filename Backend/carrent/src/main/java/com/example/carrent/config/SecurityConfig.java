2a621da896fe776987227973ea1a5b843fce7a59
Dawid Flis <df301671@student.polsl.pl>
2024-06-02 16:37:06 +0200

    webtoken

--- Backend/carrent/src/main/java/com/example/carrent/config/SecurityConfig.java
+++ Backend/carrent/src/main/java/com/example/carrent/config/SecurityConfig.java
@@ -3,54 +3,68 @@ package com.example.carrent.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.example.carrent.admin.AdminDetailsService;
+import com.example.carrent.webtoken.JwtAuthenticationFilter;
+import com.example.carrent.webtoken.MyUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 	@Autowired
-	private AdminDetailsService adminDetailsService;
+	private MyUserDetailsService myUserDetailsService;
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
-				.csrf(AbstractHttpConfigurer::disable)
-				.authorizeHttpRequests(registry -> {
-					registry.requestMatchers("/home", "/register/**", "/register").permitAll();
-					registry.requestMatchers("/admin/**").hasRole("ADMIN");
-					registry.requestMatchers("/employee/**").hasRole("USER");
-					registry.requestMatchers("/api/vehicle/**").hasAnyRole("USER", "ADMIN");
-					registry.anyRequest().authenticated();
-				})
-				.formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-				.build();
-	}
+	@Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	 @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/home", "/register/**", "/authenticate").permitAll();
+                    registry.requestMatchers("/admin/**").hasRole("ADMIN");
+                    registry.requestMatchers("/employee/**").hasRole("EMPLOYEE");
+					registry.requestMatchers("/api/brand/**").hasRole("ADMIN");
+					registry.requestMatchers("/api/model/**").hasRole("ADMIN");
+                    registry.anyRequest().authenticated();
+                })
+                //.formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
 
 	@Bean
 	public UserDetailsService userDetailsService() {
-		return adminDetailsService;
+		return myUserDetailsService;
 	}
 
 	@Bean
 	AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(adminDetailsService);
+		provider.setUserDetailsService(myUserDetailsService);
 		provider.setPasswordEncoder(passwordEncoder());
 		return provider;
 
 	}
+
+	@Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(authenticationProvider());
+    }
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
