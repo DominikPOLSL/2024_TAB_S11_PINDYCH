2a15f557864f3ee30a478fceb17b12917133d1a0
Dominik Pindych <dp300860@student.polsl.pl>
2024-06-02 07:56:02 +0200

    Finding reservation using rent

--- Backend/carrent/src/main/java/com/example/carrent/employee/EmployeeController.java
+++ Backend/carrent/src/main/java/com/example/carrent/employee/EmployeeController.java
@@ -1,12 +1,19 @@
 package com.example.carrent.employee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/employee")
@@ -71,4 +78,6 @@ public class EmployeeController {
     public ArrayList<Employee>  getEmployeesByAttribute(@PathVariable("data") String data) {
         return employeeService.findByAttribute(data);
     }
+    
+    
 }
