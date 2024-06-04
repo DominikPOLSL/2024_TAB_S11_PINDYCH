5acc77e6274bcb1b6c030aade6b46ced5c3e3ead
Dawid Flis <df301671@student.polsl.pl>
2024-06-04 13:02:48 +0200

    returning id in authorization

--- Backend/carrent/src/main/java/com/example/carrent/webtoken/AuthenticateController.java
+++ Backend/carrent/src/main/java/com/example/carrent/webtoken/AuthenticateController.java
@@ -2,6 +2,7 @@ package com.example.carrent.webtoken;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.carrent.admin.Admin;
+import com.example.carrent.admin.AdminRepository;
+import com.example.carrent.employee.Employee;
+import com.example.carrent.employee.EmployeeRepository;
+
 @RestController
 public class AuthenticateController {
 
@@ -23,6 +29,10 @@ public class AuthenticateController {
     private JwtService jwtService;
     @Autowired
     private MyUserDetailsService myUserDetailsService;
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @PostMapping("/authenticate")
     public Map<String, String> authenticateAndGetToken(@RequestBody LoginForm loginForm) {
@@ -32,6 +42,19 @@ public class AuthenticateController {
         if (authentication.isAuthenticated()) {
             UserDetails userDetails = myUserDetailsService.loadUserByUsername(loginForm.username());
             String token = jwtService.generateToken(userDetails);
+            String id = "";
+
+            Optional<Employee> employee = employeeRepository.findByEmployeeLogin(loginForm.username());
+            if (employee.isPresent()) {
+                id = String.valueOf(employee.get().getEmployeeId());
+
+            }
+
+            Optional<Admin> admin = adminRepository.findByAdminLogin(loginForm.username());
+            if (admin.isPresent()) {
+                id = String.valueOf(admin.get().getAdminId());
+
+            }
 
             String role = userDetails.getAuthorities().stream()
                     .map(GrantedAuthority::getAuthority)
@@ -41,6 +64,7 @@ public class AuthenticateController {
             Map<String, String> response = new HashMap<>();
             response.put("token", token);
             response.put("role", role);
+            response.put("id", id);
             return response;
         } else {
             throw new UsernameNotFoundException("Invalid credentials");
